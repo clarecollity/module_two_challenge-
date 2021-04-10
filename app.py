c932eeecd0b91346d7d8a8e8733a20c9e440d701
Clare <ccollity@gmail.com>
2021-04-10 15:47:01 -0700

    save csv code

--- app.py
+++ app.py
@@ -109,7 +109,10 @@ def save_qualifying_loans(qualifying_loans):
         qualifying_loans (list of lists): The qualifying bank loans.
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
-    # YOUR CODE HERE!
+def save_csv(csvpath, data):
+    with open(csvpath, 'w', newline="") as csvfile: 
+        csvwriter = csv.writer(csvfile, delimiter=",")
+        csvwriter.writerows(data)
 
 
 def run():
